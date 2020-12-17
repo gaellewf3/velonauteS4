a671f42792a65b0c1c0020d13da31af15e2ccfe2
gaelleWF3 <gaelle.wf3@gmail.com>
2020-12-17 17:06:18 +0100

    ajout propriete pseudo au user

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,6 +34,11 @@ class User implements UserInterface
      */
     private $password;
 
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $pseudo;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -110,5 +115,17 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(string $pseudo): self
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
     }
 }
