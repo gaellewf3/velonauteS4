7325bdcf861615bfdc765d600a1735a9718bf60d
Geoffrey <geoff2912@hotmail.fr>
2020-12-17 20:39:43 +0100

    MAJ Page Reservation

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,4 @@ On liste ici tous les fichiers CSS et JS de l'entrée
 //Récupération du fichier SCSS de l'entrée
 require('./app.scss'); 
 
-// Récupération du fichier Reservation CSS de l'entrée
-require('./css/reservation.css'); 
+
