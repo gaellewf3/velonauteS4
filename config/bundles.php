24b7b3f09b810555e81a196ed28869add2bd4d06
gaelleWF3 <gaelle.wf3@gmail.com>
2020-12-17 16:12:55 +0100

    make:entity, make:user, , make:migration, migrations:migrate,make:auth,debug:autowiring entity,debug:container doctrine orm default entity manager, require orm fixtures --dev, make:fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
