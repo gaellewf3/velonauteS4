1bd7f063f29cdeaf84bf19ffd9288072218cf9c6
Geoffrey <geoff2912@hotmail.fr>
2020-12-17 20:31:43 +0100

    Ajout page réservation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,13 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        // dd($this);
+        return $this->render('reservation.html.twig');
     }
 }
