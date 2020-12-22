4865c207e7cb831df3717df8b5a7816c0f5b3ff3
Buse ALAÇAM <buse.git@alacam.eu>
2020-12-22 16:23:44 +0100

    affichage de membre

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,10 @@ class UserController extends AbstractController
     /**
      * @Route("/admin/user", name="admin_user")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         return $this->render('admin/user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $userRepository->findAll()
         ]);
     }
 }
