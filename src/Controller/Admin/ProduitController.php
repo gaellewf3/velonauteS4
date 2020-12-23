7966698e3b91282fc2887ed8ad8eb74b897bb0d4
Buse ALAÇAM <buse.git@alacam.eu>
2020-12-23 17:15:05 +0100

    ajoute suppresion produit

--- src/Controller/Admin/ProduitController.php
+++ src/Controller/Admin/ProduitController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Produit;
 use App\Form\ProduitFormType;
+use App\Form\ConfirmationFormType;
 use App\Repository\ProduitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -42,6 +43,29 @@ class ProduitController extends AbstractController
         return $this->render('admin/produit/produit_edit.html.twig', [
             'produit' => $produit,
             'produit_form' => $form->createView(),
+        ]);
+    }
+
+        /**
+     * Suppresion 
+     * @Route("admin/produit/{id}/delete", name="produit_delete")
+     */
+    public function produitDelete(Produit $produit, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(ConfirmationFormType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->remove($produit);
+            $manager->flush();
+
+            $this->addFlash('info', sprintf('Le produit "%s" a bien été supprimé.', $produit->getNom()));
+            return $this->redirectToRoute('admin_produit');
+        }
+
+        return $this->render('admin/produit/produit_delete.html.twig', [
+            'produit' => $produit,
+            'confirmation_form' => $form->createView(),
         ]);
     }
 }
