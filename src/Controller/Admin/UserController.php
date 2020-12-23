e9b9292d43cbe65dd863f634ccb9d213c14bdc5b
Buse ALAÇAM <buse.git@alacam.eu>
2020-12-23 12:02:39 +0100

    ajoute de suppression de la page user

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use App\Form\UserFormType;
+use App\Form\ConfirmationFormType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,11 +35,37 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $manager->flush();
+            $this->addFlash('success', 'Le membre a été mis à jour.');
+            return $this->redirectToRoute('admin_user');
+
         }
 
         return $this->render('admin/user/user_edit.html.twig', [
             'user' => $user,
             'user_form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Suppresion d'un user
+     * @Route("admin/user/{id}/delete", name="user_delete")
+     */
+    public function userDelete(User $user, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(ConfirmationFormType::class);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->remove($user);
+            $manager->flush();
+
+            $this->addFlash('info', sprintf('Le membre "%s" a bien été supprimé.', $user->getNom()));
+            return $this->redirectToRoute('admin_user');
+        }
+
+        return $this->render('admin/user/user_delete.html.twig', [
+            'user' => $user,
+            'confirmation_form' => $form->createView(),
         ]);
     }
 }
